c15220debfce920b8e2c8b9773f48ad74723d584
Eniko Kosztolanyi <32710850+3n1k0@users.noreply.github.com>
2023-08-08 09:40:12 +0200

    update footer component and tests

--- components/footer/Footer.jsx
+++ components/footer/Footer.jsx
@@ -1,15 +1,63 @@
+import React from "react";
+import Link from "next/link";
+import { FaFacebook, FaTwitter, FaInstagram } from "react-icons/fa";
 import styles from "./Footer.module.scss";
 
-
 const Footer = () => {
-    return (
-        <div className={styles.footerWrapper}>
-            <div className={styles.footerList}>
-            <p>this is the footer</p>
-            <p>it will have important footer information</p>
-            </div>
+  return (
+    <div className={styles.footerWrapper}>
+      <div className={styles.footerContent}>
+        <div className={styles.footerColumn}>
+          <div className={styles.footerItem}>
+            <Link className={styles.footerLink} href="/categories">
+              Categories
+            </Link>
+          </div>
+          <div className={styles.footerItem}>
+            <Link className={styles.footerLink} href="/about">
+              About
+            </Link>
+          </div>
+          <div className={styles.footerItem}>
+            <Link className={styles.footerLink} href="/blog">
+              Blog
+            </Link>
+          </div>
         </div>
-    );
+        <div className={styles.footerColumn}>
+          <div className={styles.footerItem}>
+            <Link className={styles.footerLink} href="/privacy">
+              Privacy
+            </Link>
+          </div>
+          <div className={styles.footerItem}>
+            <Link className={styles.footerLink} href="/terms">
+              Terms
+            </Link>
+          </div>
+          <div className={styles.footerItem}>
+            <Link className={styles.footerLink} href="/security">
+              Security
+            </Link>
+          </div>
+        </div>
+      </div>
+      <div className={styles.footerSocialIcons}>
+        <a href="#" data-testid="facebook-icon" className={styles.socialIcon}>
+          <FaFacebook />
+        </a>
+        <a href="#" data-testid="twitter-icon"  className={styles.socialIcon}>
+          <FaTwitter />
+        </a>
+        <a href="#" data-testid="instagram-icon"  className={styles.socialIcon}>
+          <FaInstagram />
+        </a>
+      </div>
+      <p className={styles.footerCopyright}>
+        &copy; 2023 Your Company Name. All rights reserved.
+      </p>
+    </div>
+  );
 };
 
 export default Footer;
---
+++ components/footer/Footer.test.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import Footer from "./Footer";
+
+describe("Footer", () => {
+  it("renders navigation links", () => {
+    render(<Footer />);
+
+    const categoriesLink = screen.getByRole("link", { name: /categories/i });
+    const aboutLink = screen.getByRole("link", { name: /about/i });
+    const blogLink = screen.getByRole("link", { name: /blog/i });
+    const privacyLink = screen.getByRole("link", { name: /privacy/i });
+    const termsLink = screen.getByRole("link", { name: /terms/i });
+    const securityLink = screen.getByRole("link", { name: /security/i });
+
+    expect(categoriesLink).toBeInTheDocument();
+    expect(aboutLink).toBeInTheDocument();
+    expect(blogLink).toBeInTheDocument();
+    expect(privacyLink).toBeInTheDocument();
+    expect(termsLink).toBeInTheDocument();
+    expect(securityLink).toBeInTheDocument();
+  });
+
+  it("renders social icons", () => {
+    render(<Footer />);
+
+    const facebookIcon = screen.getByTestId("facebook-icon");
+    const twitterIcon = screen.getByTestId("twitter-icon");
+    const instagramIcon = screen.getByTestId("instagram-icon");
+
+    expect(facebookIcon).toBeInTheDocument();
+    expect(twitterIcon).toBeInTheDocument();
+    expect(instagramIcon).toBeInTheDocument();
+  });
+
+  it("renders copyright information", () => {
+    render(<Footer />);
+
+    const copyrightText = screen.getByText(
+      /© 2023 Your Company Name. All rights reserved./i
+    );
+
+    expect(copyrightText).toBeInTheDocument();
+  });
+});
